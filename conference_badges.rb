d57a9c4bf0631fd892d7608eeadb3d55c0998cfb
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-19 17:18:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 def assign_rooms(speakers)
   badge[]
     speakers.each_with_index do |speaker|
-    .push("Hello, #{speaker}! You'll be assigned to room #{} ")
+    badge.push("Hello, #{speaker}! You'll be assigned to room #{} ")
   end 
 end

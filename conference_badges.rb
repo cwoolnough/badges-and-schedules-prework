d40778a98faa3328e3f97c05a3c22720f2f5ce1c
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-19 17:01:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge = []
+  array.each do |array|
+    badge.push("Hello, my name is #{array}.")
+  end
+  return badge
+end
+
+def assign_rooms
+  
+end
